235cd6924b56f37ea314de4c31dde881ab717ee1
Techzoro17 <techzoro17@gmail.com>
2024-08-26 19:16:55 +0530

    Second commit

--- Script.js
+++ Script.js
@@ -9,9 +9,20 @@ function show() {
     Task.forEach(element => {
         const div = document.createElement('div');
         const data =document.createElement('h3');
+        const input = document.createElement('input');
+        input.setAttribute('type', 'checkbox'); 
+        input.checked = element.check
+        input.addEventListener("click",()=>toggle(element.id))
+        div.appendChild(input);
+
+
         data.textContent =element.task; 
         data.classList.add("data_h2")
+        if(element.check){
+            data.classList.add("strikethrough")
+        }
         div.appendChild(data)
+        
 
         const del = document.createElement('button');
         del.textContent = 'delete';
@@ -26,13 +37,25 @@ function show() {
     });
     
 }
+function toggle(id){
+ Task=Task.map((item)=>{
+    if(item.id===id){
+        item.check=!item.check
+    }
+    return item
+    
+    
+ })
+ show();
+}
 function addTask(event) {
     event.preventDefault();
     const ttask = document.getElementById('Task').value;
     if(ttask.trim()!==""){
     const newtask ={
         id:Date.now(),
-        task:ttask
+        task:ttask,
+        check:false
     }
     Task.push(newtask);
     
